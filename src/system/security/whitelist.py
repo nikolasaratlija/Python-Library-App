c99e9c9aa3cd5f57a2fd6f5cac824c83742f084d
nikolasaratlija <saratlijanikola@gmail.com>
2022-06-11 01:06:43 +0200

    whitelist functions

--- src/system/security/whitelist.py
+++ src/system/security/whitelist.py
@@ -1,16 +1,30 @@
 import re
 
 
-def password(input):
-    pass
+def validate_input(string, regex=None):
+    if is_valid_length(string) is False:
+        return False
+
+    if regex:
+        if match_regex(regex, string) is False:
+            return False
+
+    return True  # string passed all checks
 
 
-reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$"
-match_re = re.compile(reg)
+PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$"
+PHONE_REGEX = "^[0-9]{6}$"
+EMAIL_REGEX = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+ZIPCODE_REGEX = "^[0-9]{4}[A-Z]{2}$"
 
-res = re.search(match_re, "asd@Asd1")
 
-if res:
-    print("Valid Password")
-else:
-    print("Invalid Password")
+# check is string is under 255 characters
+def is_valid_length(string):
+    return len(string) < 255
+
+
+# utility function for checking whether a string matches a pattern
+def match_regex(regex, string):
+    pattern = re.compile(regex)
+    res = re.match(pattern, string)
+    return bool(res)
