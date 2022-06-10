0d0644f609499f7b36ee9e5496b4d36282acd414
nikolasaratlija <saratlijanikola@gmail.com>
2022-06-11 01:37:38 +0200

    added all possible menu options

--- src/user_interface/super_admin_menu.py
+++ src/user_interface/super_admin_menu.py
@@ -4,11 +4,24 @@ from .system_admin_menu import SystemAdminMenu
 class SuperAdminMenu(SystemAdminMenu):
     def __init__(self):
         super().__init__()
-        self._add_menu_option(lambda: print("Super Admin Option 1 Executed"), "Super Admin Menu Option 1")
-        self._add_menu_option(lambda: print("Super Admin Option 2 Executed"), "Super Admin Menu Option 2")
-        self._add_menu_option(lambda: print("Super Admin Option 3 Executed"), "Super Admin Menu Option 3")
+        self._add_menu_option(self.add_admin, "Add New Admin")
+        self._add_menu_option(self.update_admin, "Update Existing Admin")
+        self._add_menu_option(self.delete_admin, "Delete Admin")
+        self._add_menu_option(self.reset_admin_password, "Reset Admin Password")
 
     def run(self):
         self._title(f"Super Admin Menu")
         self._display_options()
         self._read_input()
+
+    def add_admin(self):
+        pass
+
+    def update_admin(self):
+        pass
+
+    def delete_admin(self):
+        pass
+
+    def reset_admin_password(self):
+        pass
--- src/user_interface/system_admin_menu.py
+++ src/user_interface/system_admin_menu.py
@@ -1,14 +1,44 @@
 from .advisor_menu import AdvisorMenu
+from src.system.roles.super_admin import SuperAdmin
 
 
 class SystemAdminMenu(AdvisorMenu):
     def __init__(self):
         super().__init__()
-        self._add_menu_option(lambda: print("System Admin Option 1 Executed"), "System Admin Menu Option 1")
-        self._add_menu_option(lambda: print("System Admin Option 2 Executed"), "System Admin Menu Option 2")
-        self._add_menu_option(lambda: print("System Admin Option 3 Executed"), "System Admin Menu Option 3")
+        self._add_menu_option(self.add_advisor, "Add New Advisor")
+        self._add_menu_option(self.modify_advisor, "Modify Existing Advisor")
+        self._add_menu_option(self.delete_advisor, "Delete Advisor")
+        self._add_menu_option(self.rest_advisor_password, "Reset Advisor Password")
+        self._add_menu_option(self.read_all_users, "List All Users")
+        self._add_menu_option(self.delete_member, "Delete Member")
+        self._add_menu_option(self.backup, "Back-Up System")
+        self._add_menu_option(self.read_logs, "Read logs")
 
     def run(self):
         self._title(f"System Admin Menu")
         self._display_options()
         self._read_input()
+
+    def add_advisor(self):
+        pass
+
+    def modify_advisor(self):
+        pass
+
+    def delete_advisor(self):
+        pass
+
+    def rest_advisor_password(self):
+        pass
+
+    def read_all_users(self):
+        pass
+
+    def delete_member(self):
+        pass
+
+    def backup(self):
+        pass
+
+    def read_logs(self):
+        pass
