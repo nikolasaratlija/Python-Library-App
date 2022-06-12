42207e5738c774afb07f6e20660095287c5f2d2e
nikolasaratlija <saratlijanikola@gmail.com>
2022-06-12 18:25:21 +0200

    revert app entrypoint

--- ffs.py
+++ ffs.py
@@ -1,9 +1,5 @@
 from src.user_interface.login_menu import LoginMenu
-from src.user_interface.super_admin_menu import SuperAdminMenu
 
-menu = SuperAdminMenu()
-menu.run()
-
-# if __name__ == "__main__":
-#     login = LoginMenu()
-#     login.run()
+if __name__ == "__main__":
+    login = LoginMenu()
+    login.run()
