d7f194b269fb790f21899ca8f391b0ba32115f45
nikolasaratlija <saratlijanikola@gmail.com>
2022-06-18 12:14:22 +0200

    user id generator

--- ffs.py
+++ ffs.py
@@ -3,14 +3,18 @@ from src.user_interface.login_menu import LoginMenu
 from src.user_interface.super_admin_menu import SuperAdminMenu
 from src.system.context import Context
 
-Context.user_id = 1
-Context.user_name = 'nikola'
-Context.db_connection = get_connection()
-
-menu = SuperAdminMenu()
-menu.run()
+# Context.user_id = 1
+# Context.user_name = 'nikola'
+# Context.db_connection = get_connection()
+#
+# menu = SuperAdminMenu()
+# menu.run()
 
 # if __name__ == "__main__":
 #     Context.db_connection = get_connection()
 #     login = LoginMenu()
 #     login.run()
+
+from src.system.security.user_id_generator import *
+
+print(generate_user_id())
---
+++ src/system/security/user_id_generator.py
@@ -0,0 +1,30 @@
+from random import randint
+
+
+def generate_user_id():
+    """ Keeps looping until it finds a valid id """
+
+    while True:
+        new_id = _generate_random_id()
+        if _check_id_validity(new_id):
+            return int_list_to_string(new_id)
+
+
+def _generate_random_id():
+    first_number = randint(1, 9)  # generates 1 random number between 1 and 9
+    number_list = [randint(0, 9) for _ in range(9)]  # generates list of 9 random numbers between 0 and 9
+    number_list.insert(0, first_number)  # joins all numbers to create a list of 10 numbers
+    return number_list
+
+
+def _check_id_validity(number_list):
+    sum_digits = sum(number_list[:-1])
+    if sum_digits % 10 == number_list[-1]:  # checks the sum of the numbers against the last digit modulo 10
+        return True
+    else:
+        return False
+
+
+def int_list_to_string(int_list):
+    string_ints = [str(number) for number in int_list]
+    return "".join(string_ints)
